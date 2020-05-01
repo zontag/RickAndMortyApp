5eb95683ba0ffb603f8079e66452c958c8a6cd5e
Tiago Louis Zontag <tlzontag@icloud.com>
2020-05-01 08:08:15 -0300

    First commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Tiago Louis Zontag on 01/05/20.
+//  Copyright © 2020 Tiago Louis Zontag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
